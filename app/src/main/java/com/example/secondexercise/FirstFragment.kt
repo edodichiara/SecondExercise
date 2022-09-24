62ea5fa52f8d0316072e37a8ea528d84406ae958
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-09-24 16:33:25 +0200

    Add setOnClickListener

--- app/src/main/java/com/example/secondexercise/FirstFragment.kt
+++ app/src/main/java/com/example/secondexercise/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.secondexercise.databinding.FragmentFirstBinding
 
 
@@ -15,13 +16,21 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         bindingFirst = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.buttonFirstFragment.setOnClickListener {
+            if (binding.editTextFirstFragment.text.contains("@")){
+                binding.emailText.text = binding.editTextFirstFragment.text
+                Toast.makeText(context, resources.getString(R.string.toast_text), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, resources.getString(R.string.email_not_valid_error), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
